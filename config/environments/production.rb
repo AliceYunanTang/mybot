3e2e351beae5e36b6b308c6680c72fc5aed7ffdb
AliceYunanTang <tangyunan@hotmail.com>
2017-08-13 11:13:11 +1000

    added url(asset-path('background-image.jpg')), for  testing heroku background

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
-
+# added below four lines to try to make heroku to show background-image without success 13Aug2017
 config.serve_static_files = true
 config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
 config.assets.compile = true
